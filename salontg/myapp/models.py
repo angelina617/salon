db49041496fa0f060a79658e5e61f7fa247b3070
Angelina Pogorelova <sergeevna617@mail.ru>
2026-01-30 12:32:16 +0700

    tables

--- salontg/myapp/models.py
+++ salontg/myapp/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    ROLE_CHOICES = [
+        ('client', 'Клиент'),
+        ('master', 'Мастер'),
+        ('admin', 'Администратор'),
+    ]
+    
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    phone = models.CharField(max_length=11, verbose_name='Телефон')
+    email = models.EmailField(verbose_name='Email')
+    password = models.CharField(max_length=50, verbose_name='Пароль')
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, verbose_name='Роль')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.role})"
+
+class Services(models.Model):
+    CATEGORY_CHOICES = [
+        ('coloring','Окрашивание'),
+        ('manicure', 'Маникюр'),
+        ('massage', 'Массаж'),
+        ('cosmetology', 'Косметология'),
+    ]
+    name = models.CharField(max_length=50, verbose_name='Название услуги')
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, verbose_name='Категория')
+    duration = models.IntegerField(verbose_name='Длительность (мин)')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+class Masters(models.Model):
+    
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, verbose_name='Пользователь', related_name='master_profile')
+    specialization = models.CharField(max_length=50 , verbose_name='Специализация')
+    experience = models.IntegerField(verbose_name='Стаж')
+    description = models.TextField(verbose_name='О мастере')
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+class Appointments(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Ожидает подтверждение'),
+        ('confirmed', 'Подтверждено'),
+        ('complated', 'Завершена'),
+        ('cancelled', 'Отменена'),
+        ('no_show', 'Не пришёл'),
+    ]
+
+    client = models.ForeignKey(Users, on_delete=models.CASCADE, verbose_name='Клиент', related_name='appointments')
+    master = models.ForeignKey(Masters, on_delete=models.CASCADE, verbose_name='Мастер', related_name='appointments')  
+    service = models.ForeignKey(Services, on_delete=models.CASCADE, verbose_name='Услуга', related_name='appointments')    
+    date = models.DateField(verbose_name='Дата')
+    time = models.TimeField(verbose_name='Время')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending', verbose_name='Статус')
+
+    def __str__(self):
+        return f"{self.client} - {self.service} ({self.date} {self.time})"
