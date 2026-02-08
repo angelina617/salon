547ffd0d6c7a74f8a1fe81aa90a18c28047f353d
Angelina Pogorelova <sergeevna617@mail.ru>
2026-02-08 19:18:51 +0700

    edit

--- salontg/salontg/settings.py
+++ salontg/salontg/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'salontg.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'myapp' /'templates'],
+        'DIRS': [BASE_DIR /'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
