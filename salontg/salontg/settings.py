1af9ead8e4205c0236c48ec78b495cde69353e71
Angelina Pogorelova <sergeevna617@mail.ru>
2026-02-10 22:47:51 +0700

    updd

--- salontg/salontg/settings.py
+++ salontg/salontg/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'salontg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'salon_crasoti',
+        'USER': 'postgres',
+        'PASSWORD': '103690',
+        'HOST': 'localhost', 
+        'PORT': '5432',
+        },
     }
-}
+
 
 
 # Password validation
